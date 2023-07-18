514480a10e9e1bc5110530f6347add923850f00d
Som Shrivastava <som.shrivastava@gmail.com>
2023-07-18 09:09:35 -0400

    done pt 4

--- morse.py
+++ morse.py
@@ -118,7 +118,7 @@ class Solution:
                 if message[i] == " ":
                     new_message += "  "
                 else:
-                    new_message += MORSE_CODE_DICT[message[i]]
+                    new_message += MORSE_CODE_DICT[message[i]] + " "
             return new_message[0:len(new_message) - 1]
 
 def main():
